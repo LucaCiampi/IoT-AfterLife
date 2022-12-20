72c5fcdd1a0505b8f42c743ba5cc02761851a010
Luca <luca.ciampi@edu.gobelins.fr>
2022-12-20 16:01:03 +0100

    pasted bluetooth peripheral code from interaction 3 to 4 and 5

--- AfterLife/TabBar/TabInteraction4/MainInteraction4View.swift
+++ AfterLife/TabBar/TabInteraction4/MainInteraction4View.swift
@@ -8,8 +8,76 @@
 import SwiftUI
 
 struct MainInteraction4View: View {
+    @EnvironmentObject var bleInterface: BLEObservable
+    
+    @State var connectionString = "No device connected"
+    @State var scanButtonString = "Start scan"
+    @State var isScanningDevices = false
+    @State var isShowingDetailView = false
+    
+    var esp32Interaction3Name = "rfid-luca"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text(connectionString)
+            
+            if bleInterface.connectedPeripheral != nil {
+                Button("Disconnect") {
+                    bleInterface.disconnectFrom(p: bleInterface.connectedPeripheral!)
+                }
+            }
+            
+            List(bleInterface.periphList.reversed().filter({ $0.name == esp32Interaction3Name })) { p in
+                SinglePeripheralView(periphName: p.name).onTapGesture {
+                    bleInterface.connectTo(p: p)
+                }
+            }
+            
+            if (isShowingDetailView) {
+                VStack {
+                    Text("Détails manipulation ici")
+                }
+            }
+            
+            HStack {
+                Button(scanButtonString) {
+                    isScanningDevices = !isScanningDevices
+                    if (isScanningDevices) {
+                        scanButtonString = "Stop scan"
+                        bleInterface.startScan()
+                    }
+                    else {
+                        scanButtonString = "Start scan"
+                        bleInterface.stopScan()
+                    }
+                }
+            }
+        }.onChange(of: bleInterface.connectedPeripheral, perform: { newValue in
+            if let p = newValue {
+                connectionString = p.name
+            }
+            else {
+                connectionString = "No ESP32 connected"
+            }
+        }).onChange(of: bleInterface.connectionState, perform: { newValue in
+            switch newValue {
+                
+            case .disconnected:
+                isShowingDetailView = false
+                break
+            case .connecting:
+                connectionString = "Connecting... "
+                break
+            case .discovering:
+                connectionString = "Discovering... "
+                break
+            case .ready:
+                connectionString = "Connected to " + connectionString
+                isShowingDetailView = true
+                break
+            }
+        })
+        .padding()
     }
 }
 
